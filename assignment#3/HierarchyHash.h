53e87a8e74ad46c2b76e247a9a88ca21c6be1806
uni20151295 <junseok11@unist.ac.kr>
2018-11-15 11:09:03 +0900

    "assignment#3"

---
+++ assignment#3/HierarchyHash.h
@@ -0,0 +1,102 @@
+#ifndef HIERARCHYHASH_H
+#define HIERARCHYHASH_H
+
+#include <iostream>
+#include "FlatHash.h"
+
+class HierarchyHash
+{
+private:
+	unsigned int** hashtable;
+	// Variable for overflow handling
+	enum overflow_handle flag;
+	// Size(range) of full hash table. Initially 1000
+	unsigned int table_size;
+	// Size of subhash table. Fixed by 100
+	unsigned int sub_table_size;
+	// Nums of keys
+	unsigned int num_of_keys;
+
+public:
+	HierarchyHash(enum overflow_handle _flag);
+
+	~HierarchyHash();
+
+	unsigned int HashFunction(const int key) { return key % table_size; }
+
+	unsigned int GetTableSize() { return table_size; }
+
+	unsigned int GetNumofKeys() { return num_of_keys; }
+
+	// Return the size of allocated sub hash table
+	unsigned int GetAllocatedSize();
+
+	// Return time cost
+	int Insert(const unsigned int key);
+
+	// Return time cost
+	int Remove(const unsigned int key);
+
+	// Return time cost
+	int Search(const unsigned int key);
+
+	void Print();
+};
+
+HierarchyHash::HierarchyHash(enum overflow_handle _flag)
+{
+	// Initial map size is 1000
+	table_size = 1000;
+	// Table size is fixed to 100
+	sub_table_size = 100;
+	flag = _flag;
+
+	// Write your code
+
+
+}
+
+HierarchyHash::~HierarchyHash()
+{
+	// Write your code
+
+}
+
+unsigned int HierarchyHash::GetAllocatedSize()
+{
+	// Write your code
+
+}
+
+int HierarchyHash::Insert(const unsigned int key)
+{
+	// Write your code
+
+}
+
+int HierarchyHash::Remove(const unsigned int key)
+{
+	// Write your code
+
+}
+
+int HierarchyHash::Search(const unsigned int key)
+{
+	// Write your code
+
+}
+
+void HierarchyHash::Print()
+{
+	// Print valid key pair for each sub hash table - subtable_id:[index1:key1,index2:key2]
+	// Seperate each sub table by endl
+	// e.g., 0:[1:3,3:7,5:1]
+	//       1:[101:2,192:10]
+	//       9:[902:90,938:82]
+
+	// Write your code
+
+
+}
+
+#endif
---
+++ assignment#3/NRKFlat.h
@@ -0,0 +1,118 @@
+#ifndef NRKFLAT_H
+#define NRKFLAT_H
+
+#include "FlatHash.h"
+#include <iostream>
+
+#define INT2VOIDP(i) (void*)(uintptr_t)(i)
+
+class NRKFlat : public FlatHash
+{
+private:
+	// counter array
+	unsigned int* counters;
+
+public:
+	NRKFlat(enum overflow_handle _flag);
+
+	~NRKFlat();
+	
+	// Hash function
+	unsigned int MurmurHash2(const void* key);
+
+	void GetMMHashValue(const unsigned int key, int& h1, int& h2, int& h3);
+
+	bool Filter(const unsigned int key);
+
+	// Overwriting
+	int Remove(const unsigned int key);
+
+	// Overwriting
+	int Search(const unsigned int key);
+};
+
+NRKFlat::NRKFlat(enum overflow_handle _flag) : FlatHash(_flag)
+{
+	// Write your code
+
+}
+
+NRKFlat::~NRKFlat()
+{
+	// Write your code
+
+}
+
+unsigned int NRKFlat::MurmurHash2(const void* key){
+	int len = 4;
+	unsigned int seed = 2018;
+
+	const unsigned int m = 0x5bd1e995;
+	const int r = 24;
+
+	unsigned int h = seed ^ len;
+
+	const unsigned char * data = (const unsigned char *)key;
+
+	while(len >= 4)
+	{
+		unsigned int k = *(unsigned int *)data;
+
+		k *= m; 
+		k ^= k >> r; 
+		k *= m; 
+		
+		h *= m; 
+		h ^= k;
+
+		data += 4;
+		len -= 4;
+	}
+
+	switch(len)
+	{
+	case 3: h ^= data[2] << 16;
+	case 2: h ^= data[1] << 8;
+	case 1: h ^= data[0];
+	        h *= m;
+	};
+
+	h ^= h >> 13;
+	h *= m;
+	h ^= h >> 15;
+
+	return h;
+}
+
+void NRKFlat::GetMMHashValue(const unsigned int key, int& h1, int& h2, int& h3)
+{	
+	// You can use h1, h2 and h3 as hashing results which you have to use to decide counter locations
+	h1 = MurmurHash2(INT2VOIDP(key));
+	h2 = MurmurHash2(INT2VOIDP(h1));
+	h3 = MurmurHash2(INT2VOIDP(h2));
+}
+
+bool NRKFlat::Filter(const unsigned int key)
+{
+	// You can use h1, h2 and h3 as hashing results which you have to use to decide counter locations
+	int h1, h2, h3;
+	GetMMHashValue(key, h1, h2, h3);
+
+	// Write your code
+
+	
+}
+
+int NRKFlat::Remove(const unsigned int key)
+{
+	// Write your code
+
+}
+
+int NRKFlat::Search(const unsigned int key)
+{
+	// Write your code
+
+}
+
+#endif
