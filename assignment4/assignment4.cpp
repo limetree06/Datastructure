a365f97af3ae5165b8ecd0aa975f2cacee5be7ba
sonminji <limetree06@unist.ac.kr>
2018-12-20 11:29:06 +0000

    Update assignment4.cpp

--- assignment4/assignment4.cpp
+++ assignment4/assignment4.cpp
@@ -1,9 +1,3 @@
-//
-// assignment4.h  (Version 1.0)
-//
-// CSE221 Data Structure, Fall 2018
-//
-
 #include <iostream>
 #include <string>
 #include <stdexcept>
@@ -17,19 +11,39 @@
 
 
 double FlightMap::calcRouteDistance(const list<string> route) {
+    double alldistance = 0;
+    int i=0;
+    list<string>::const_iterator itor;
+    list<string>::const_iterator preitor;
+    itor=route.begin();
 
+    while(itor != route.end()||i<10000){
+        i++;
+        itor++;
 
+        if(itor != route.begin()){
+            if(isConnectionExist(*preitor, *itor)==false)
+            throw runtime_error("error");
+
+            alldistance += *(airport_db[*preitor].outgoingEdge(airport_db[*itor]));
+      }
+        if(isAirportExist(*itor)==false)
+            throw runtime_error("error");
+            preitor = itor;
+    }
+
+    return alldistance;
 }
 
 
 list<string> FlightMap::findShortestRoute(const string &airport1, const string &airport2) {
+    if(isAirportExist(airport1)==false) throw runtime_error("error");
+    if(isAirportExist(airport2)==false) throw runtime_error("erroe");
 
 
 }
 
 
 void FlightMap::printAllShortestRoutes(const string &airport) {
-
-
-}
-
+    if(isAirportExist(airport)==false) throw runtime_error("error");
+  }
